104ebf1d853ed68e3c5d9881ce3e6d1c1c47a4ae
Prabhjit Singh <pj92singh@gmail.com>
2019-09-29 19:38:59 -0400

    Update Btooth.java

--- BT android/Btooth.java
+++ BT android/Btooth.java
@@ -26,39 +26,103 @@ val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
 
 */
 
-public class Btooth(){
+package com.example.bttest;
 
-    public setupBtooh(){
-        //if the adapter is null then not supported?
-        //
-        var bluetoothHeadset: BluetoothHeadset? = null
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-        // Get the default adapter
-        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-        private val profileListener = object : BluetoothProfile.ServiceListener {
+import java.util.Set;
 
-            override fun onServiceConnected(profile: Int, proxy: BluetoothProfile) {
-                if (profile == BluetoothProfile.HEADSET) {
-                    bluetoothHeadset = proxy as BluetoothHeadset
+/*pj92singh
+* Prabhjit Singh
+* Blueetooth test */
+public class MainActivity extends AppCompatActivity {
+    private static int GET_BLUETOOTH_ON = 100;
+    private boolean per_Granted = false;
+    public  static int REQUEST_ENABLE_BT = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*
+        * first get the permission for bluetooth
+        * */
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case GET_BLUETOOTH_ON;
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    per_Granted = true;
+                }else {
+                    per_Granted = false;
+                    Toast.makeText(getApplicationContext(), "App needs bluetooth premission!!", Toast.LENGTH_LONG).show();
                 }
-            }
+                break;
+        }
+    }
 
-            override fun onServiceDisconnected(profile: Int) {
-                if (profile == BluetoothProfile.HEADSET) {
-                    bluetoothHeadset = null
+    public void getBt(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            Toast.makeText(getApplicationContext(), "permission denied", Toast.LENGTH_LONG);
+
+        }
+    }
+    /*
+    * after Bluetooth permission is granted we can connect the 2 devices
+    * then proceed to stream data/media
+    * */
+    public void PairDevices(){
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (!bluetoothAdapter.isEnabled())
+        {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+                if (bluetoothAdapter == null)
+                {
+                    // Device doesn't support Bluetooth
+                    Log.i("DEBUG", "bluetooth not connected/device not supported");
+                    Toast.makeText(getApplicationContext(), "device not supported", Toast.LENGTH_LONG);
+
+                } else{
+                    //bluetooth is enabled on device
+                    //find paried devices
+                    Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+                    if (pairedDevices.size() > 0)
+                    {
+                        // There are paired devices. Get the name and address of each paired device.
+                        for (BluetoothDevice device : pairedDevices)
+                        {
+                            String deviceName = device.getName();
+                            String deviceHardwareAddress = device.getAddress(); // MAC address
+                            Log.i("DEBUG BT", "BT list: " + deviceName + " hardwareAddress: " + deviceHardwareAddress);
+                        }
+                    }else if (pairedDevices.size() < 0){
+                        //no devices, need to discover
+                        //scan for devices
+                    }
                 }
-            }
         }
 
-        // Establish connection to the proxy.
-        bluetoothAdapter?.getProfileProxy(context, profileListener, BluetoothProfile.HEADSET)
-
-        // ... call functions on bluetoothHeadset
-
-        // Close proxy connection after use.
-        bluetoothAdapter?.closeProfileProxy(BluetoothProfile.HEADSET, bluetoothHeadset)
-
-      }
-
+    }
 }
